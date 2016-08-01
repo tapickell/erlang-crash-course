9c32c4a148feaa9c0aae97b57e987763bee9df62
Todd Pickell <tapickell@gmail.com>
2016-08-01 16:47:51 -0500

    done w/ kitty gen server on to FSM

---
+++ kitty-app/src/kitty_gen_server.erl
@@ -0,0 +1,53 @@
+-module(kitty_gen_server).
+-compile(export_all).
+-behaviour(gen_server).
+-record(cat, { name
+             , color=green
+             , description
+             }).
+
+start_link() -> gen_server:start_link(?MODULE, [], []).
+
+%% Synch Call
+order_cat(Pid, Name, Color, Description) ->
+  gen_server:call(Pid, {order, Name, Color, Description}).
+
+%% Async Cast
+return_cat(Pid, Cat=#cat{}) ->
+  gen_server:cast(Pid, {return, Cat}).
+
+%% Synch Call
+close_shop(Pid) ->
+  gen_server:call(Pid, terminate).
+
+%% Server Functions
+init([]) -> {ok, []}.
+
+handle_call({order, Name, Color, Description}, _From, Cats) ->
+  if Cats =:= [] ->
+       {reply, make_cat(Name, Color, Description), Cats};
+     Cats =/= [] ->
+       {reply, hd(Cats), tl(Cats)}
+  end;
+
+handle_call(terminate, _From, Cats) ->
+  {stop, normal, ok, Cats}.
+
+handle_cast({return, Cat=#cat{}}, Cats) ->
+  {noreply, [Cat|Cats]}.
+
+handle_info(Msg, Cats) ->
+  io:format("Unexpected message: ~p~n", [Msg]),
+  {noreply, Cats}.
+
+terminate(normal, Cats) ->
+  [io:format("~p was set free.~n", [C#cat.name]) || C <- Cats],
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% Private Functions
+make_cat(Name, Color, Description) ->
+  #cat{name=Name, color=Color, description=Description}.
+
