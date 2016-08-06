98fd33919974f42c007b96a533ae47b097d461d6
Todd Pickell <tapickell@gmail.com>
2016-08-05 23:40:41 -0500

    done with FSM on to event handlers

--- trade_fsm.erl
+++ trade_fsm.erl
@@ -58,6 +58,7 @@ ready(OwnPid) ->
 cancel(OwnPid) ->
   gen_fsm:sync_send_all_state_event(OwnPid, cancel).
 
+
 %% async negotiation functions
 ask_negotiate(OtherPid, OwnPid) ->
   gen_fsm:send_event(OtherPid, {ask_negotiate, OwnPid}).
@@ -83,6 +84,7 @@ am_ready(OtherPid) ->
 ack_trans(OtherPid) ->
   gen_fsm:send_event(OtherPid, ack).
 
+
 %% synch negotiation functions
 ask_commit(OtherPid) ->
   gen_fsm:sync_send_event(OtherPid, ask_commit).
@@ -93,6 +95,7 @@ do_commit(OtherPid) ->
 %% all state event
 notify_cancel(OtherPid) ->
   gen_fsm:send_all_state_event(OtherPid, cancel).
+
 
 %% fsm callbacks
 -record(state, { name=""
@@ -106,12 +109,62 @@ notify_cancel(OtherPid) ->
 init(Name) ->
   {ok, idle, #state{name=Name}}.
 
+
+handle_event(cancel, _StateName, S=#state{}) ->
+  notice(S, "recieved cancel event", []),
+  {stop, other_cancelled, S};
+
+handle_event(Event, StateName, Data) ->
+  unexpected(Event, StateName),
+  {next_state, StateName, Data}.
+
+
+handle_sync_event(cancel, _From, _StateName, S=#state{}) ->
+  notify_cancel(S#state.other),
+  notice(S, "Cancelling trade, sending cancel event", []),
+  {stop, cancelled, ok, S};
+
+handle_sync_event(Event, _From, StateName, Data) ->
+  unexpected(Event, StateName),
+  {next_state, StateName, Data}.
+
+
+handle_info({'DOWN', Ref, process, Pid, Reason}, _, S=#state{other=Pid, monitor=Ref}) ->
+  notice(S, "Other side is dead", []),
+  {stop, {other_down, Reason}, S};
+
+handle_info(Info, StateName, Data) ->
+  unexpected(Info, StateName),
+  {next_state, StateName, Data}.
+
+
+code_change(_OldVsn, StateName, Data, _Extra) ->
+  {ok, StateName, Data}.
+
+
+terminate(normal, ready, S=#state{}) ->
+  notice(S, "FSM leaving.", []);
+
+terminate(_Reason, _StateName, _StateData) ->
+  ok.
+
+
 notice(#state{name=N}, Str, Args) ->
   io:format("~s: "++Str++"~n", [N|Args]).
 
 unexpected(Msg, State) ->
   io:format("~p received unkown event ~p while in state ~p~n"
             , [self(), Msg, State]).
+
+priority(OwnPid, OtherPid) when OwnPid > OtherPid -> true;
+priority(OwnPid, OtherPid) when OwnPid < OtherPid -> false.
+
+commit(S=#state{}) ->
+  io:format("Transaction completed for ~s. "
+            "Items sent are:~n~p,~n received are:~n~p.~n"
+            "This operation should have some atomic save "
+            "in a database.~n",
+            [S#state.name, S#state.ownitems, S#state.otheritems]).
 
 add(Item, Items) ->
   [Item | Items].
@@ -123,36 +176,162 @@ idle({ask_negotiate, OtherPid}, S=#state{}) ->
   Ref = monitor(process, OtherPid),
   notice(S, "~p asked for a trade negotiation", [OtherPid]),
   {next_state, idle_wait, S#state{other=OtherPid, monitor=Ref}};
+
 idle(Event, Data) ->
   unexpected(Event, idle),
   {next_state, idle, Data}.
+
 
 idle({negotiate, OtherPid}, From, S=#state{}) ->
   ask_negotiate(OtherPid, self()),
   notice(S, "asking user ~p for a trade", [OtherPid]),
   Ref = monitor(process, OtherPid),
   {next_state, idle_wait, S#state{other=OtherPid, monitor=Ref, from=From}};
+
 idle(Event, _From, Data) ->
   unexpected(Event, idle),
   {next_state, idle, Data}.
+
 
 idle_wait({ask_negotiate, OtherPid}, S=#state{other=OtherPid}) ->
   gen_fsm:reply(S#state.from, ok),
   notice(S, "starting negotiation", []),
   {next_state, negotiate, S};
+
 idle_wait({accept_negotiate, OtherPid}, S=#state{other=OtherPid}) ->
   gen_fsm:reply(S#state.from, ok),
   notice(S, "starting negotiation", []),
   {next_state, negotiate, S};
+
 idle_wait(Event, Data) ->
   unexpected(Event, idle_wait),
   {next_state, idle_wait, Data}.
+
 
 idle_wait(accept_negotiate, _From, S=#state{other=OtherPid}) ->
   accept_negotiate(OtherPid, self()),
   notice(S, "accepting negotiation", []),
   {reply, ok, negotiate, S};
+
 idle_wait(Event, _From, Data) ->
   unexpected(Event, idle_wait),
   {next_state, idle_wait, Data}.
+
+
+negotiate({make_offer, Item}, S=#state{ownitems=OwnItems}) ->
+  do_offer(S#state.other, Item),
+  notice(S, "offering ~p", [Item]),
+  {next_state, negotiate, S#state{ownitems=add(Item, OwnItems)}};
+
+negotiate({retract_offer, Item}, S=#state{ownitems=OwnItems}) ->
+  undo_offer(S#state.other, Item),
+  notice(S, "cancelling offer on ~p", [Item]),
+  {next_state, negotiate, S#state{ownitems=remove(Item, OwnItems)}};
+
+negotiate({do_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  notice(S, "other player offereing ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=add(Item, OtherItems)}};
+
+negotiate({undo_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  notice(S, "Other player cancelling offer on ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=remove(Item, OtherItems)}};
+
+negotiate(are_you_ready, S=#state{other=OtherPid}) ->
+  io:format("Other user ready to trade.~n"),
+  notice(S, "Other user ready to transfer goods:~n"
+            "You get ~p, The other side gets ~p",
+            [S#state.otheritems, S#state.ownitems]),
+  not_yet(OtherPid),
+  {next_state, negotiate, S};
+
+negotiate(Event, Data) ->
+  unexpected(Event, negotiate),
+  {next_state, negotiate, Data}.
+
+
+negotiate(ready, From, S=#state{other=OtherPid}) ->
+  are_you_ready(OtherPid),
+  notice(S, "asking if ready, waiting", []),
+  {next_state, wait, S#state{from=From}};
+
+negotiate(Event, _From, S) ->
+  unexpected(Event, negotiate),
+  {next_state, negotiate, S}.
+
+
+wait({do_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  gen_fsm:reply(S#state.from, offer_changed),
+  notice(S, "other side offering ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=add(Item, OtherItems)}};
+
+wait({undo_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  gen_fsm:reply(S#state.from, offer_changed),
+  notice(S, "other side cancelling offer of ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=remove(Item, OtherItems)}};
+
+wait(are_you_ready, S=#state{}) ->
+  am_ready(S#state.other),
+  notice(S, "asked if ready, and I am. Waiting for same reply", []),
+  {next_state, wait, S};
+
+wait(not_yet, S=#state{}) ->
+  notice(S, "Other not ready yet", []),
+  {next_state, wait, S};
+
+wait('ready!', S=#state{}) ->
+  am_ready(S#state.other),
+  ack_trans(S#state.other),
+  gen_fsm:reply(S#state.from, ok),
+  notice(S, "other side is ready. Moving to ready state", []),
+  {next_state, ready, S};
+
+wait(Event, Data) ->
+  unexpected(Event, wait),
+  {next_state, wait, Data}.
+
+
+ready(ack, S=#state{}) ->
+  case priority(self(), S#state.other) of
+    true ->
+      try
+        notice(S, "asking for commit", []),
+        ready_commit = ask_commit(S#state.other),
+        notice(S, "ordering commit", []),
+        ok = do_commit(S#state.other),
+        notice(S, "committing...", []),
+        commit(S),
+        {stop, normal, S}
+      catch Class:Reason ->
+              notice(S, "commit failed", []),
+              {stop, {Class, Reason}, S}
+      end;
+    false ->
+      {next_state, ready, S}
+  end;
+
+ready(Event, Data) ->
+  unexpected(Event, ready),
+  {next_state, ready, Data}.
+
+ready(ask_commit, _From, S) ->
+  notice(S, "replying to ask_commit", []),
+  {reply, ready_commit, ready, S};
+
+ready(do_commit, _From, S) ->
+  notice(S, "committing...", []),
+  commit(S),
+  {stop, normal, ok, S};
+
+ready(Event, _From, Data) ->
+  unexpected(Event, ready),
+  {next_state, ready, Data}.
+
+
+
+
+
+
+
+
+
 
